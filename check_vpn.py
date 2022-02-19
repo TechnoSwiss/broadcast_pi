5b71beeb0ae5e96f523adc249d950248fe715d53
TechnoSwiss <technoswiss@typhoondigital.com>
2022-02-18 23:44:24 -0800

    Updated text notifications to be sent if VPN restart failed, instead of on VPN restart to reduce the number of text messages.

--- check_vpn.py
+++ check_vpn.py
@@ -4,14 +4,32 @@ import argparse
 import os
 import traceback
 import json
+import sys
+import time
 
 from subprocess import Popen, check_output, PIPE
 from datetime import datetime, timedelta
 
 import sms # sms.py local file
 
+FAILURES_TO_RESTART = 0
 ATTEMPTS_PER_HOUR = 2
 ATTEMPTS_PER_DAY  = 6
+DELAY_AFTER_RESTART = 20
+
+def is_vpn_connected(pc_name, num_from = None, num_to = None, verbose = False):
+    try:
+        ip_addr = check_output(['ip', 'addr']).decode('utf-8')
+
+        if('tun' not in ip_addr):
+            return False
+        else:
+            return True
+    except:
+        if(verbose): print(traceback.format_exc())
+        if(num_from is not None and num_to is not None):
+            sms.send_sms(num_from, num_to, pc_name + " failed to verify or restart VPN!", verbose)
+        sys.exit()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Restart VPN if not running.')
@@ -23,44 +41,53 @@ if __name__ == '__main__':
 
     restart_attempts = os.path.abspath(os.path.dirname(__file__)) + '/vpn_restart'
 
-    try:
-        ip_addr = check_output(['ip', 'addr']).decode('utf-8')
-
-        if('tun' not in ip_addr):
-            if(os.path.exists(restart_attempts)):
-                with open(restart_attempts, "r") as attemptsFile:
-                    try:
-                        attempts = json.load(attemptsFile)
-                    except:
-                        if(args.verbose): print(traceback.format_exc())
-                        attempts = {}
-            else:
-                attempts = {}
-            attempt_num = attempts['num_attempts'] if 'num_attempts' in attempts else 0
-            attempt_first = datetime.fromisoformat(attempts['first_attempt'])  if 'first_attempt' in attempts else datetime.now()
-            time_diff = datetime.now() - attempt_first
-            max_attempts_hour = (time_diff.seconds//3600 + 1) * ATTEMPTS_PER_HOUR
-            max_attempts_day = (time_diff.days + 1) * ATTEMPTS_PER_DAY
-            if(args.verbose): print("Attempts per Hour: " + str(max_attempts_hour))
-            if(args.verbose): print("Attempts per Day: " + str(max_attempts_day))
-            if(args.verbose): print("Attempt Num: " + str(attempt_num))
-            if(attempt_num < max_attempts_hour and attempt_num < max_attempts_day):
-                with open(restart_attempts, "w") as attemptsFile:
-                    if(args.num_from is not None and args.num_to is not None):
-                        sms.send_sms(args.num_from, args.num_to, args.pc_name + " VPN down, attempting to restart!", args.verbose)
-                    print("Restarting VPN.")
-                    # user account must have passwordless sudo access to restart the OpenVPN service for this to work (ie update sudoers 'ALL=NOPASSWD: /bin/systemctl restart openvpn')
-                    systemctl_results = check_output(['sudo', 'systemctl', 'restart', 'openvpn'])
-                    attempt_num += 1
-                    attempts['num_attempts'] = attempt_num
-                    attempts['first_attempt'] = attempt_first
-                    json.dump(attempts, attemptsFile, indent=4, sort_keys=True, default=str)
+    if(not is_vpn_connected(args.pc_name, args.num_from, args.num_to, args.verbose)):
+        if(os.path.exists(restart_attempts)):
+            with open(restart_attempts, "r") as attemptsFile:
+                try:
+                    attempts = json.load(attemptsFile)
+                except:
+                    if(args.verbose): print(traceback.format_exc())
+                    attempts = {}
+        else:
+            attempts = {}
+        failure_num = attempts['num_failures'] if 'num_failures' in attempts else 1
+        attempt_num = attempts['num_attempts'] if 'num_attempts' in attempts else 0
+        attempt_first = datetime.fromisoformat(attempts['first_attempt'])  if 'first_attempt' in attempts else datetime.now()
+        time_diff = datetime.now() - attempt_first
+        max_attempts_hour = (time_diff.seconds//3600 + 1) * ATTEMPTS_PER_HOUR
+        max_attempts_day = (time_diff.days + 1) * ATTEMPTS_PER_DAY
+        if(args.verbose): print("Failure Num: " + str(failure_num))
+        if(args.verbose): print("Attempts per Hour: " + str(max_attempts_hour))
+        if(args.verbose): print("Attempts per Day: " + str(max_attempts_day))
+        if(args.verbose): print("Attempt Num: " + str(attempt_num))
+        vpn_restart = False
+        if(failure_num > FAILURES_TO_RESTART and attempt_num < max_attempts_hour and attempt_num < max_attempts_day):
+            vpn_restart = True
+            #if(args.num_from is not None and args.num_to is not None):
+            #    sms.send_sms(args.num_from, args.num_to, args.pc_name + " VPN down, attempting to restart!", args.verbose)
+            print("Restarting VPN.")
+            # user account must have passwordless sudo access to restart the OpenVPN service for this to work (ie update sudoers 'ALL=NOPASSWD: /bin/systemctl restart openvpn')
+            systemctl_results = check_output(['sudo', 'systemctl', 'restart', 'openvpn'])
+            attempt_num += 1
+        with open(restart_attempts, "w") as attemptsFile:
+            failure_num += 1
+            attempts['num_failures'] = failure_num
+            attempts['num_attempts'] = attempt_num
+            attempts['first_attempt'] = attempt_first
+            json.dump(attempts, attemptsFile, indent=4, sort_keys=True, default=str)
+        if(vpn_restart):
+            # we want to verify the VPN came back up, and only text about a problem if it did not
+            restart_time = datetime.now()
+            while((datetime.now() - restart_time).seconds < DELAY_AFTER_RESTART):
+                time.sleep(1)
+                if(args.verbose): print('Seconds since restart : ' + str((datetime.now() - restart_time).seconds), end='\r')
+            if(not is_vpn_connected(args.pc_name, args.num_from, args.num_to, args.verbose)):
+                if(args.num_from is not None and args.num_to is not None):
+                    sms.send_sms(args.num_from, args.num_to, args.pc_name + " VPN failed to restart!", args.verbose)
+                print("VPN failed to restart.")
         else:
             # VPN is up so clean up VPN restart status file
             if os.path.exists(restart_attempts):
                 os.remove(restart_attempts)
-    except:
-        if(args.verbose): print(traceback.format_exc())
-        if(args.num_from is not None and args.num_to is not None):
-            sms.send_sms(args.num_from, args.num_to, args.pc_name + " failed to verify or restart VPN!", args.verbose)
 
