9c4ed0a081d916d811381c277ecd6502ef10d142
TechnoSwiss <technoswiss@typhoondigital.com>
2024-10-11 17:47:35 -0700

    Updates to send_email.py to help with debugging the functions, allows pulling in the json files instead of filling out all parameters on the CLI Updates the send_total_views to not use a multipart message, since we're not attaching anything, and leaving it as MIMEMultitype will cause the attachment icon to show on some email client

--- send_email.py
+++ send_email.py
@@ -5,6 +5,7 @@ import os
 import traceback
 import argparse
 import traceback
+import json
 from datetime import datetime
 import time
 
@@ -48,12 +49,11 @@ def send_total_views(email_from, email_to, ward, total_views, dkim_private_key =
     # so need to be able to turn it into a comma seperated string
     if(type(email_to) == list):
         email_to = ",".join(email_to)
-    msg = MIMEMultipart()
+    msg = MIMEText("There were " + total_views + " total view(s) reported by YouTube.", 'plain')
     msg["From"] = email_from
     msg["To"] = email_to
     msg["Subject"] = datetime.now().strftime("%A %b. ") + ordinal(datetime.now().strftime("%-d")) + " Broadcast final view(s) count - " + ward.replace("_", " ")
     msg["Message-ID"] = "<" + str(time.time()) + "-" + email_from + ">"
-    msg.attach(MIMEText("There were " + total_views + " total view(s) recorded by YouTube.", 'plain'))
 
     try:
         if(dkim_private_key and dkim_selector):
@@ -94,7 +94,7 @@ def send_viewer_file(csv_file, email_from, email_to, ward, total_views, dkim_pri
     msg["To"] = email_to
     msg["Subject"] = datetime.now().strftime("%A %b. ") + ordinal(datetime.now().strftime("%-d")) + " Broadcast - " + ward.replace("_", " ")
     msg["Message-ID"] = "<" + str(time.time()) + "-" + email_from + ">"
-    msg.attach(MIMEText("There were " + total_views + " total view(s) recorded by YouTube.\nFor breakdown of concurrent viewers during broadcast,\nplease open the attached file in a spreadsheet app. (Excel/Google Docs).", 'plain'))
+    msg.attach(MIMEText("There were " + total_views + " view(s) reported by YouTube.\nFor breakdown of concurrent viewers during broadcast,\nplease open the attached file in a spreadsheet app. (Excel/Google Docs).", 'plain'))
 
     try:
         with open(csv_file) as fp:
@@ -132,9 +132,10 @@ def send_viewer_file(csv_file, email_from, email_to, ward, total_views, dkim_pri
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Email CSV file')
-    parser.add_argument('-w','--ward',type=str,required=True,help='Name of Ward being broadcast')
-    parser.add_argument('-e','--email-from',type=str,required=True,help='Account to send email with/from')
-    parser.add_argument('-E','--email-to',type=str,required=True,help='Accoun tto send CSV fiel email to')
+    parser.add_argument('-c','--config-file',type=str,help='JSON Configuration file')
+    parser.add_argument('-w','--ward',type=str,help='Name of Ward being broadcast')
+    parser.add_argument('-e','--email-from',type=str,help='Account to send email with/from')
+    parser.add_argument('-E','--email-to',type=str,help='Accoun tto send CSV fiel email to')
     parser.add_argument('-M','--dkim-private-key',type=str,help='Full path and filename of DKIM private key file')
     parser.add_argument('-m','--dkim-selector',type=str,help='DKIM Domain Selector')
     parser.add_argument('-F','--num-from',type=str,help='SMS notification from number - Twilio account number')
@@ -143,4 +144,46 @@ if __name__ == '__main__':
     parser.add_argument('-v','--verbose',default=False, action='store_true',help='Increases vebosity of error messages')
     args = parser.parse_args()
 
-    send_viewer_file(args.viewers_file, args.email_from, args.email_to, args.ward, 0, args.dkim_private_key, args.dkim_selector, args.num_from, args.num_to, args.verbose)
+    verbose = args.verbose
+    ward = args.ward
+
+    if(args.config_file is not None):
+        if("/" in args.config_file):
+            config_file = args.config_file
+        else:
+            config_file =  os.path.abspath(os.path.dirname(__file__)) + "/" + args.config_file
+    if(verbose): print('Config file : ' + config_file)
+    if(config_file is not None and os.path.exists(config_file)):
+        with open(config_file, "r") as configFile:
+            config = json.load(configFile)
+
+            # check for keys in config file
+            if 'broadcast_ward' in config:
+                ward = config['broadcast_ward']
+            if 'email_from_account' in config:
+                args.email_from = config['email_from_account']
+            if 'email_dkim_key' in config:
+                args.dkim_private_key = config['email_dkim_key']
+            if 'email_dkim_domain' in config:
+                args.dkim_selector = config['email_dkim_domain']
+            if 'email_viewer_addresses' in config:
+                args.email_to = config['email_viewer_addresses']
+            if 'notification_text_from' in config:
+                num_from = config['notification_text_from']
+            if 'notification_text_to' in config:
+                num_to = config['notification_text_to']
+
+    if(ward is None):
+        print("!!Ward is a required argument!!")
+        exit()
+
+    if(args.email_from is None):
+        print("!!Email from is a required argument!!")
+        exit()
+
+    if(args.email_to is None):
+        print("!!Email To is a required argument!!")
+        exit()
+
+    send_total_views(args.email_from, args.email_to, ward, '0', args.dkim_private_key, args.dkim_selector, args.num_from, args.num_to, args.verbose)
+    send_viewer_file(args.viewers_file, args.email_from, args.email_to, ward, '0', args.dkim_private_key, args.dkim_selector, args.num_from, args.num_to, args.verbose)
